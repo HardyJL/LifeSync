daecb72e5d851c972f95e9f6c66bbb0432142b14
HardyJL <j.hardenberg@hotmail.de>
2024-02-01 22:15:36 +0100

    initial layouts

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Life Sync',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: lifeSyncGreen),
         useMaterial3: true,
       ),
-      home: const Center(),
+      home: Scaffold(
+          // appBar: AppBar(
+          //   title: Text(
+          //     'Life Sync',
+          //     style: Theme.of(context).textTheme.headlineLarge,
+          //   ),
+
+          // ),
+          body: CustomScrollView(
+        physics: const AlwaysScrollableScrollPhysics(
+            parent: BouncingScrollPhysics()),
+        slivers: [
+          SliverAppBar(
+            // expandedHeight: 125,
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_horiz),
+              ),
+              const SizedBox(
+                width: 8,
+              )
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                'Life Sync',
+                style: Theme.of(context).textTheme.headlineLarge,
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
+
+MaterialColor lifeSyncGreen = const MaterialColor(
+  0xFF4CAF50,
+  <int, Color>{
+    50: Color(0xFFE8F5E9),
+    100: Color(0xFFC8E6C9),
+    200: Color(0xFFA5D6A7),
+    300: Color(0xFF81C784),
+    400: Color(0xFF66BB6A),
+    500: Color(0xFF4CAF50), // Primary Color
+    600: Color(0xFF43A047),
+    700: Color(0xFF388E3C),
+    800: Color(0xFF2E7D32),
+    900: Color(0xFF1B5E20),
+  },
+);
